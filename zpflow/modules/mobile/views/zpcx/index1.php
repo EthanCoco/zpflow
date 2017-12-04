931577d56aca4c2d85ffe44142dc00e0d26d32bc
lijianlin <lijianlin0204@163.com>
2017-12-04 22:12:06 +0800

    模块-手机端招聘查询-暂无招聘信息页面注释

--- zpflow/modules/mobile/views/zpcx/index1.php
+++ zpflow/modules/mobile/views/zpcx/index1.php
@@ -1,3 +1,8 @@
+<!--
+	作者：lijianlin0204@163.com
+	时间：2017-12-04
+	描述：暂无招聘信息界面
+-->
 <div class="mobile-index2-content-index1">
 	<p style='font-size: 15px;text-align:center;font-family:\"微软雅黑\";'>
 		暂时没有招聘信息，敬请期待下一次招聘！
