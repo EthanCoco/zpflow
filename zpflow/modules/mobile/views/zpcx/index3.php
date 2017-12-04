2a0135e7d43ced51557ccd8b89ba6002aa2cc18e
lijianlin <lijianlin0204@163.com>
2017-12-04 22:13:48 +0800

    模块-手机端正在招聘界面注释

--- zpflow/modules/mobile/views/zpcx/index3.php
+++ zpflow/modules/mobile/views/zpcx/index3.php
@@ -1,3 +1,8 @@
+<!--
+	作者：lijianlin0204@163.com
+	时间：2017-12-04
+	描述：正在招聘界面
+-->
 <div style="padding: 10px;margin-bottom: 60px;text-align: center;">
 	<p style="padding: 10px;">当前存在<?= $recInfo['recYear'] ?>年，<?= $recInfo['recBatch'] ?>正在招聘......</p>
 	<p style="padding: 10px;">报名起始时间：<?= $recInfo['recStart'] ?></p>
@@ -8,7 +13,9 @@
 </div>
 
 <script>
+/*报名*/
 function mobile_entry(){
+	//加载报名界面
 	$("#index2_content").load("<?= yii\helpers\Url::to(['zpcx/entry']); ?>");
 }
 </script>
